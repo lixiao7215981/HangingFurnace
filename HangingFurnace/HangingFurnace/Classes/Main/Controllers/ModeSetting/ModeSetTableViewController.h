e4905529bb750b6cef7650fc906cdd14c0788b86
ybyao07 <ybyao07@gmail.com>
2015-09-07 10:35:49 +0800

    add modesettingcell

---
+++ HangingFurnace/HangingFurnace/Classes/Main/Controllers/ModeSetting/ModeSetTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModeSetTableViewController.h
+//  HangingFurnace
+//
+//  Created by ybyao07 on 15/9/7.
+//  Copyright (c) 2015年 skyware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModeSetTableViewController : UITableViewController
+
+@end
