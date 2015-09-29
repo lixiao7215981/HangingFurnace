5c7fee580e8850b283252da8c93e4909bda87ae8
ybyao07 <ybyao07@gmail.com>
2015-09-29 14:08:33 +0800

    h

--- HangingFurnace/HangingFurnace/Classes/Main/Other/AppDelegate.m
+++ HangingFurnace/HangingFurnace/Classes/Main/Other/AppDelegate.m
@@ -71,7 +71,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-//    [MQTT_Tool ]
+    MQTT_Tool 
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
